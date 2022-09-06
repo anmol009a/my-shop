2448b446dfb65ba0e53afb9fdbf3fae87e9e171e
Anmol Sharma <anmolsharma009a@gmail.com>
2022-09-06 12:49:34 +0530

    fixed bug

--- backend/src/api/order/controllers/custom.js
+++ backend/src/api/order/controllers/custom.js
@@ -5,9 +5,9 @@ const https = require('https');
 const PaytmChecksum = require('paytmchecksum');
 
 
-module.exports = createCoreController('api::order.order', ({ strapi, env }) => ({
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
   // Method 1: Creating an entirely custom action
-  async exampleAction(ctx,env) {
+  async exampleAction(ctx) {
     /*
     * import checksum generation utility
     * You can get this utility from https://developer.paytm.com/docs/checksum/
@@ -17,7 +17,7 @@ module.exports = createCoreController('api::order.order', ({ strapi, env }) => (
 
     paytmParams.body = {
       "requestType": "Payment",
-      "mid": env("MID"),
+      "mid": process.env.MID,
       "websiteName": "YOUR_WEBSITE_NAME",
       "orderId": "ORDERID_98765",
       "callbackUrl": "http://localhost:1337/api/orders/posttransaction",
@@ -34,48 +34,52 @@ module.exports = createCoreController('api::order.order', ({ strapi, env }) => (
     * Generate checksum by parameters we have in body
     * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
     */
-    PaytmChecksum.generateSignature(JSON.stringify(paytmParams.body), env("MKey")).then(function (checksum) {
+    let checksum = await PaytmChecksum.generateSignature(JSON.stringify(paytmParams.body), process.env.MKEY);
 
-      paytmParams.head = {
-        "signature": checksum
-      };
+    paytmParams.head = {
+      "signature": checksum
+    };
+    var post_data = JSON.stringify(paytmParams);
 
-      var post_data = JSON.stringify(paytmParams);
+    const getToken = async () => {
+      return new Promise((resolve, reject) => {
+        var options = {
+          /* for Staging */
+          hostname: 'securegw-stage.paytm.in',
 
-      var options = {
+          /* for Production */
+          // hostname: 'securegw.paytm.in',
 
-        /* for Staging */
-        hostname: 'securegw-stage.paytm.in',
+          port: 443,
+          path: `/theia/api/v1/initiateTransaction?mid=${process.env.MID}&orderId=ORDERID_98765`,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': post_data.length
+          }
+        };
 
-        /* for Production */
-        // hostname: 'securegw.paytm.in',
+        var response = "";
+        var post_req = https.request(options, function (post_res) {
+          post_res.on('data', function (chunk) {
+            response += chunk;
+          });
 
-        port: 443,
-        path: `/theia/api/v1/initiateTransaction?mid=${env("MID")}&orderId=ORDERID_98765`,
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': post_data.length
-        }
-      };
-
-      var response = "";
-      var post_req = https.request(options, function (post_res) {
-        post_res.on('data', function (chunk) {
-          response += chunk;
+          post_res.on('end', function () {
+            console.log('Response: ', response);
+            resolve(response);
+          });
         });
 
-        post_res.on('end', function () {
-          console.log('Response: ', response);
-        });
-      });
+        post_req.write(post_data);
+        post_req.end();
+      })
+    }
 
-      post_req.write(post_data);
-      post_req.end();
-    });
+    let myResponse = await getToken()
+    ctx.body = myResponse;
+    // ctx.send(JSON.parse(myResponse))
+
   },
-
-
-
 
 }));
